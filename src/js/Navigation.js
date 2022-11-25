b911b1a436a90aba1ad4c5d13f91ed42c8dda35b
Aiyumi3 <rsomeonei@gmail.com>
2022-11-25 21:22:28 +0200

    commit

--- src/js/Navigation.js
+++ src/js/Navigation.js
@@ -12,6 +12,13 @@ import r6 from '../images/Rectangle-6.svg';
 import r7 from '../images/Rectangle-7.svg';
 import r8 from '../images/Rectangle-8.svg';
 import vector from '../images/Vector 1.svg';
+import arrowDown from '../images/arrowdown.png';
+import neon1 from '../images/whiteNeon.svg';
+import neon2 from '../images/blueNeon.svg';
+import neon3 from '../images/pinkNeon.svg';
+import neon4 from '../images/greenNeon.svg';
+import spline from '../images/spline1.svg';
+import spline2 from '../images/spline2.svg';
 
 //
 export default function Header() {
@@ -29,7 +36,26 @@ export default function Header() {
                 <img src={r6}  className="rect6" alt="bg"  />
                 <img src={r7}  className="rect7" alt="bg"  />
                 <img src={r8}  className="rect8" alt="bg"  />
-                <p className="welcome">~WELCOME~</p>
+                <div className="welcome">
+                    <p className="wOut" >~WELCOME~</p>
+                </div>
+                <div className="welcome2">
+                    <p className="" >make your day more <br/> productive with TO DO</p>
+                </div>
+                <div className="spline">
+                    <img src={spline}  className="spline1" alt="spline"  />
+                    <img src={spline2}  className="spline2" alt="spline"  />
+                </div>
+                <div className="arrowDown">
+                    <img src={arrowDown}  className="" alt="arrow down"  />
+                </div>
+                <div className="">
+                    <img src={neon1}  className="neon1" alt="neon"  />
+                    <img src={neon2}  className="neon2" alt="neon"  />
+                    <img src={neon3}  className="neon3" alt="neon"  />
+                    <img src={neon4}  className="neon4" alt="neon"  />
+                </div>
+
             </div>
 
         </div>
